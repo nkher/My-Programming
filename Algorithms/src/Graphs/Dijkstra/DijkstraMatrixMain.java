f4035a4d388c19f4448df49c0e8f6978b4cc938d
Namesh Kher <namesh.kher@gmail.com>
2015-10-15 01:33:51 -0400

    Added Dijkstras Shortest path algo implementation

---
+++ Algorithms/src/Graphs/Dijkstra/DijkstraMatrixMain.java
@@ -0,0 +1,23 @@
+package Graphs.Dijkstra;
+
+public class DijkstraMatrixMain {
+
+	
+	public static void main(String[] args) {
+		
+		int graphWeight[][] =  {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+				                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+				                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+				                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+				                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+				                {0, 0, 4, 0, 10, 0, 2, 0, 0},
+				                {0, 0, 0, 14, 0, 2, 0, 1, 6},
+				                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+				                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+				               };
+		
+		DijkstraAdjMatrix object = new DijkstraAdjMatrix();
+		object.dijkstra(graphWeight, 0);
+	}
+
+}
